3e0a51d1b2dcd41ffe6ced9ba96bcd491dcdc075
it1shka <2tishbel@gmail.com>
2020-12-18 12:18:49 +0300

    asdasd

--- brestscript/lang/compiler/graphlib/graph.js
+++ brestscript/lang/compiler/graphlib/graph.js
@@ -12,3 +12,10 @@ function set_pixel(x, y){
 function rect(x, y, w, h){
     ctx.fillRect(x, y, w, h);
 }
+
+function line(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
